e76f78e0e47096c4197a55ad90d54218cdd152bd
mitchmarns <sakurapizzaa@gmail.com>
2024-11-26 21:00:27 -0800

    Update team.js

--- team.js
+++ team.js
@@ -23,13 +23,11 @@ export function getAvailablePlayers() {
 export function assignPlayerToTeam(playerId, teamName) {
   const player = playersData.players.find(p => p.id === playerId);
   const team = teams.find(t => t.name === teamName);
-    if (player && !player.team && team.players.length < team.maxPlayers) {
+
+  if (player && team && !player.team) {
+    if (team.players.length < team.maxPlayers) {
       player.team = teamName;
       team.players.push(player);
-      } else {
-        console.error('Cannot assign player. Team might be full or player already assigned.');
-  }
-}
 
 
 // Save updated teams to localStorage
