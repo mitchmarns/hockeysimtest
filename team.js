3334c486029dcc4b8612095d21f15a842ca9740a
mitchmarns <sakurapizzaa@gmail.com>
2024-11-26 19:47:48 -0800

    Update team.js

--- team.js
+++ team.js
@@ -23,11 +23,11 @@ export function getAvailablePlayers() {
 export function assignPlayerToTeam(playerId, teamName) {
   const player = playersData.players.find(p => p.id === playerId);
   const team = teams.find(t => t.name === teamName);
-  if (player && !player.team && team.players.length < team.maxPlayers) {
-    player.team = teamName;
-    team.players.push(player);
-  } else {
-    console.error('Cannot assign player. Team might be full or player already assigned.');
+    if (player && !player.team && team.players.length < team.maxPlayers) {
+      player.team = teamName;
+      team.players.push(player);
+      } else {
+        console.error('Cannot assign player. Team might be full or player already assigned.');
   }
 }
 
