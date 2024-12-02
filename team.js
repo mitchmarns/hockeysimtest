ec55a7a7babb14423af7c619b93de50dac0615f9
mitchmarns <sakurapizzaa@gmail.com>
2024-12-01 19:46:00 -0800

    Update team.js

--- team.js
+++ team.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const playersContainer = document.getElementById("players-container");
   const teamFilter = document.getElementById("team-filter");
 
