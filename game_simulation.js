1ef4f914bf24beebad2d7de4601da6d883f86370
mitchmarns <sakurapizzaa@gmail.com>
2024-12-05 13:26:40 -0800

    Update game_simulation.js

--- game_simulation.js
+++ game_simulation.js
@@ -47,7 +47,7 @@ function getRandomInt(max) {
 }
 
 function calculateSkill(player) {
-  return (player.skills.glove + player.skills.stick + player.skills.legs + player.skills.speed) / 4;
+  return (player.skills.glove + player.skills.stick + player.skills.legs + player.skills.speed) / 8;
 }
 
 // Simulate period
