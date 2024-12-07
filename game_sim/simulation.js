3dd720a63081749d0dd5b3d560017930abda54d0
mitchmarns <sakurapizzaa@gmail.com>
2024-12-07 08:02:56 -0800

    Update simulation.js

--- game_sim/simulation.js
+++ game_sim/simulation.js
@@ -141,8 +141,8 @@ const simulateOvertimePlay = (homeTeam, awayTeam, gameLog) => {
     return { winner: null };
   }
 
-  const shooterSkill = scorer.skills.wristShotAccuracy * 0.4 
-                     + scorer.skills.wristShotPower * 0.3 
+  const shooterSkill = scorer.skills.wristShotAccuracy * 0.3 
+                     + scorer.skills.wristShotPower * 0.2 
                      + scorer.skills.speed * 0.2 
                      + scorer.skills.hockeyIQ * 0.1;
   const goalieSkill = goalie.skills.glove * 0.3 
