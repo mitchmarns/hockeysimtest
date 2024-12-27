5fc27e9474410b9c6cdbdd65d19661a8bcf2afa9
mitchmarns <sakurapizzaa@gmail.com>
2024-12-27 08:45:16 -0800

    Update data.js

--- game_sim/data.js
+++ game_sim/data.js
@@ -1,25 +1,7 @@
 // Load players from localStorage
-export const loadPlayersFromStorage = () => {
-  const playersData = localStorage.getItem("playersData");
-  if (playersData) {
-    try {
-      return JSON.parse(playersData).players || [];
-    } catch (error) {
-      console.error("Error parsing players data from localStorage", error);
-      return [];
-    }
-  }
-  return [];
-};
-
-// Save players to localStorage
-export const savePlayersToStorage = (players) => {
-  localStorage.setItem("playersData", JSON.stringify({ players }));
-};
-
-// Load teams from localStorage
 export const loadTeamsFromStorage = () => {
   const teamsData = localStorage.getItem("teams");
+  console.log('Loaded teams data from localStorage:', teamsData); // Log raw data
   if (teamsData) {
     try {
       return JSON.parse(teamsData) || [];
@@ -31,7 +13,7 @@ export const loadTeamsFromStorage = () => {
   return [];
 };
 
-// Save teams to localStorage
-export const saveTeamsToStorage = (teams) => {
-  localStorage.setItem("teams", JSON.stringify(teams));
+// Save players to localStorage
+export const savePlayersToStorage = (players) => {
+  localStorage.setItem("playersData", JSON.stringify({ players }));
 };
